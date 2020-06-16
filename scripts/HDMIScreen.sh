aaa80fdce90023dccf811450a1fba87a8a1f53ce
Nicolai Thagaard <59829565+NicolaiThagaard@users.noreply.github.com>
2020-06-16 10:31:47 +0200

    Delete HDMIScreen.sh

--- scripts/HDMIScreen.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --primary --mode 1366x768 --pos 0x512 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1920x1200 --pos 1366x0 --rotate normal --output DP2 --off
