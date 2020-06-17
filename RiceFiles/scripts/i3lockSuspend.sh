48b14ddaf593b536068e2cdac716bcf4b27fe76f
Nicolai Thagaard <59829565+NicolaiThagaard@users.noreply.github.com>
2020-06-17 10:15:10 +0200

    Update i3lockSuspend.sh

--- RiceFiles/scripts/i3lockSuspend.sh
+++ RiceFiles/scripts/i3lockSuspend.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#file=~/.config/scripts/lock2.png
-
-#script der sætter baggrundsbilledet alt efter hvad klokken er
-#/home/a/.config/scripts/TimeBasedTheme.sh 
 
 file=~/.config/scripts/lockNEW3.png
 
@@ -14,14 +10,7 @@ i3lock -n "${PARAM[@]}" -i "$file" >/dev/null 2>&1 &
 
 sleep 2
 printf "Your Sudo password" | base64 -d | sudo -S systemctl suspend
-sleep 5
-printf "Your Sudo Password" | base64 -d | sudo -S /home/a/dell-bios-fan-control/dell-bios-fan-control enable
-/home/a/.config/scripts/i8kctl.sh
-sleep 1
-/home/a/.config/scripts/i8kctl.sh
-sleep 10
-/home/a/.config/scripts/i8kctl.sh
-sleep 30
-/home/a/.config/scripts/i8kctl.sh
 
-#i3lock -i /home/a/baggrunde/baggrund3.jpg
+#Optional. If script is run from a Dell Latitude e5570, to disable BIOS fan control and the the system handle it.
+#sleep 5
+#printf "Your Sudo Password" | base64 -d | sudo -S /home/a/dell-bios-fan-control/dell-bios-fan-control enable
