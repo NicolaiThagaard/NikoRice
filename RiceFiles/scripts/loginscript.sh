702e302c15e7ef5ff017c9180dca1944d9e4e1c0
Nicolai Thagaard <59829565+NicolaiThagaard@users.noreply.github.com>
2020-06-17 10:10:46 +0200

    Update loginscript.sh

--- RiceFiles/scripts/loginscript.sh
+++ RiceFiles/scripts/loginscript.sh
@@ -2,7 +2,7 @@
 ##Changes ressulution to default of the hdmi monitor and disables the laptop monitor
 PATH="${PATH}:${HOME}/.local/bin/"
 #HDMICHECK=$(xrandr | grep " connected " | awk '{ print$1 }' | grep HDMI-1)
-xrdb /home/a/.Xressources
+xrdb ${HOME}/.Xressources
 #if [[ $HDMICHECK -eq "HDMI1" ]]
 #then
 #	echo "HDMI Monitor found"
@@ -11,8 +11,8 @@ xrdb /home/a/.Xressources
 #	wal -i /home/a/ibm.jpg -a 80
 #else
 #	echo "HDMI Monitor not fouind"
-	wal -i /home/a/baggrunde/macro.png -a 80
+	wal -i ${HOME}/baggrunde/macro.png -a 80
 #fi
 ##Add more stuff
 urxvt -name dropdown &> /dev/null &
-urxvt -name pulsemixer -e /home/a/.config/scripts/pulsemixer.sh &> /dev/null &
+urxvt -name pulsemixer -e ${HOME}/.config/scripts/pulsemixer.sh &> /dev/null &
